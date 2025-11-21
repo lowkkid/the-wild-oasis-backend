eb5f2e9445ec1885840fe92995ad50e9dd8b4c21
lowkkid <lowkkidvstheworld@gmail.com>
2025-11-21 12:54:04 +0300

    init

---
+++ src/main/java/com/github/lowkkid/thewildoasisbackend/TheWildOasisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.lowkkid.thewildoasisbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheWildOasisBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheWildOasisBackendApplication.class, args);
+    }
+
+}
